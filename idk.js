70e942844f3e97331672a0550d029c96cf3c2628
dotada <luboslav.blazhev@gmail.com>
2023-11-09 20:59:38 +0000

    initial commit

---
+++ idk.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    VANTA.WAVES({
+        el: "#vanta-animation",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00
+    });
+}
